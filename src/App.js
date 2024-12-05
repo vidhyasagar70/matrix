aa452919d4665237961543d46d8f9b69e1aa5589
vidhyasagar70 <vidhyas369@gmail.com>
2024-12-05 23:23:11 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Matrix = ()=>{
+  const [colors, setColors] = useState(Array(9).fill(null));
+  const [clickOrder,setClickOrder]= useState([]);
+  
+  const handleClick = (index) =>{
+    if(colors[index] === 'green') return;
 
-export default App;
+    const updatedColors =[...colors];
+    updatedColors[index]='green';
+    setColors(updatedColors);
+
+    const newClickOrder=[...clickOrder,index];
+    setClickOrder(newClickOrder);
+
+    if(index === 8){
+      let delay = 0;
+      newClickOrder.forEach((i,idx)=>{
+        setTimeout(()=>{
+          const sequenceColors=[...colors];
+          sequenceColors[i] = "orange";
+          setColors(sequenceColors);
+        },delay);
+        delay +=500;
+      });
+    }
+  };
+return(
+  <div style={{display:"grid",gridTemplateColumns:"repeat(3,100px)",gap:"10px"}}>
+
+    {
+      Array(9).fill(null).map((_,index)=>(
+        <div
+            key={index} onClick={()=> handleClick(index)}
+            style={{
+              width:"100px",
+              height:"100px",
+              backgroundColor:colors[index] || "lightgray",
+              display:"flex",
+              justifyContent:"center",
+              border:"1px solid black",
+              cursor:"pointer",
+            }}>
+            {index+1}
+          </div>
+
+      ))
+    }
+  </div>
+
+ );
+};
+export default Matrix;
